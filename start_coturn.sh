9d570e775c3c2c155c76a11d82df3ba7d20a3ae8
rubenzasa <51445047+rubenzasa@users.noreply.github.com>
2019-06-06 16:50:34 +0200

    Update start_coturn.sh

--- start_coturn.sh
+++ start_coturn.sh
@@ -2,4 +2,4 @@
 
 echo "Starting TURN/STUN server"
 
-turnserver -a -v -L 11.1.2.130 --server-name coturn --static-auth-secret=Noviembre2018.XP --realm=it2.com  -p 3478 --min-port 10000 --max-port 20010
+turnserver -a -v -L 172.16.0.121 --server-name coturn --static-auth-secret=Noviembre2018.XP --realm=it2.com  -p 3478 --min-port 10000 --max-port 20010
